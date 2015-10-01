a32d8efc4a24e3aca8def9595e5fedeff28401d9
Nicholas Ellis <nellis@invoca.com>
2015-10-01 15:01:37 -0700

    messing with global var in config

--- source/conf.py
+++ source/conf.py
@@ -102,6 +102,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 # Uncomment the following lines to build the docs locally using sphinx-build
+RTD_NEW_THEME = False
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
