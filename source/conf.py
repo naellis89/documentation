d651339e7c8f316c461361c352bc2774d17ed53d
Nicholas Ellis <nellis@invoca.com>
2015-10-01 14:29:21 -0700

    patch for rtd_theme

--- source/conf.py
+++ source/conf.py
@@ -102,9 +102,13 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 # Uncomment the following lines to build the docs locally using sphinx-build
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_rtd_theme
+  html_theme = 'sphinx_rtd_theme'
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 html_style = 'css/custom.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
